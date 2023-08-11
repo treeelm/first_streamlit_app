4a6cda8c422b908ffec9d99e52e3d1fb1f4b9f2b
treeelm <131100277+treeelm@users.noreply.github.com>
2023-08-12 04:46:25 +1000

    Update streamlit_app.py
    Removed pre-selected kiwi
    changed text to error message

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,9 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.write('The user entered ', fruit_choice)
+    streamlit.error('Please select a fruit to get infromation.')
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
